91090affc8376b953bb80a128779c912bbf3596a
kingpangilinan25 <king_pangilinan@rocketmail.com>
2017-12-09 13:18:39 +0800

    test1

--- gruntfile.js
+++ gruntfile.js
@@ -1,26 +1,40 @@
 module.exports = function(grunt) {
   grunt.initConfig({
       
-    sass: {
-      dist: {
-        options: {
-          style: 'expanded' //expanded
-        },
-        files: [{
-          src: 'components/sass/style.scss',
-          dest: 'epidemicmarketing/css/style-layout.css'
-        }]
-      }
-    }, //sass
+  sass: {
+    dist: {
+      options: {
+        style: 'expanded' //expanded
+      },
+      files: [{
+        src: 'components/sass/style.scss',
+        dest: 'epidemicmarketing/css/style-layout2.css'
+      }]
+    }
+  }, //sass
+    
+  watch: {
+    options: {
+      spawn: false,
+      //livereload: true
+    },
+    scripts: {
+      files: [
+        '**/*.js',
+        '**/*.scss'
+      ],
+      tasks: ['sass']
+    }
+  }
       
   }); //initConfig
 
   //grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-sass');
   //grunt.loadNpmTasks('grunt-contrib-connect');
-  //grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass', 'watch']);
 
 
 }; //wrapper function
